0c8132e837531303f81a7a73cf167cc04e571ba7
Ana Cristina González S. <anacristina0914@me.com>
2024-11-12 10:08:59 +0100

    Removed function to add dependencies

--- R/compute_dissimilarity_matrix.R
+++ R/compute_dissimilarity_matrix.R
@@ -7,11 +7,14 @@
 #' For further details, see help(daisy).
 #' @param metric Metric to be used to compute matrix. Possible options include "gower" (default), "euclidean", or "manhattan".
 #' @param stand if TRUE, measurements in x are standardized before calculating dissimilarities. For further details, see help(daisy).
+#' @param return_dis if TRUE function returns dissimilarity object rather than matrix.
 #' @param plot_dissim if TRUE, plots the dissimilarity matrix as a heatmap and shows hierarchical clustering on the plot.
-#' @param plot_title Title for the heatmap plot.
+#' @param heatmap_title Title for the heatmap plot.
+#' @param heatmap_axis_text_size Heatmap x-y axis text size.
 #' @param plot_title_size Font size for the plot title.
 #' @param sillplot if TRUE, calculates and stores a silhouette plot for varying numbers of clusters.
 #' @param ndim Maximum number of clusters to consider for the silhouette plot.
+#' @param save_plots If TRUE plots produced are saved in the working directory.
 #' @return A list containing the dissimilarity matrix, optionally the heatmap plot, and the silhouette plot if specified.
 #' @examples
 #' # Example of how to use the function
@@ -21,22 +24,27 @@
 #' result$silhouette_plot  # Access the silhouette plot (if sillplot = TRUE)
 #'
 #' @export
-compute_dissimilarity_matrix <- function(x, metric = c("euclidean", "manhattan", "gower"), stand = FALSE,
-                                         plot_dissim = TRUE, plot_title = paste0(metric, " Dissimilarity Matrix"),
-                                         plot_title_size = 20, sillplot = TRUE, ndim = 10, save_plots = FALSE) {
+compute_dissimilarity_matrix <- function(x, metric = c("euclidean", "manhattan", "gower"), stand = FALSE, return_dis = FALSE,
+                                         plot_dissim = TRUE, heatmap_title = paste0(metric, " Dissimilarity Matrix"), heatmap_axis_text_size = 4,
+                                         plot_title_size = 20, sillplot = TRUE, ndim = 10,
+                                         save_plots = FALSE) {
 
   # Calculate the dissimilarity matrix
   x_disim <- daisy(x, metric = metric, stand = stand)
   x_dissim_matrix <- as.matrix(x_disim)
 
-  # Initialize a list to store results
-  result <- list(dissimilarity_matrix = x_dissim_matrix)
+  if (return_dis){
+    # Initialize a list to store results
+    result <- list(dissimilarity_matrix = x_disim)
+  } else {
+    result <- list(dissimilarity_matrix = x_dissim_matrix)
+  }
 
   # Optionally create a heatmap plot
   if (plot_dissim) {
     pheatmap_dissim <- factoextra::fviz_dist(x_disim) +
-      theme(axis.text = element_text(size = 9), plot.title = element_text(size = plot_title_size)) +
-      ggtitle(plot_title)
+      theme(axis.text = element_text(size = heatmap_axis_text_size), plot.title = element_text(size = plot_title_size)) +
+      ggtitle(heatmap_title)
 
     # Add the plot to the list
     result$heatmap_plot <- pheatmap_dissim
@@ -59,9 +67,9 @@ compute_dissimilarity_matrix <- function(x, metric = c("euclidean", "manhattan",
     sil_data <- data.frame(Clusters = factor(1:ndim), Avg_Silhouette_Width = sil_width)
     sillplot <- ggplot(sil_data, aes(x = Clusters, y = Avg_Silhouette_Width, group = 1)) +
       geom_line() +
-      geom_point(colour = "steelblue", size = 8) +
+      geom_point(colour = "steelblue", size = 6) +
       labs(x = "Number of clusters", y = "Average Silhouette Width") +
-      theme_minimal()
+      theme_linedraw()
 
     # Add silhouette plot to the list
     result$silhouette_plot <- sillplot
