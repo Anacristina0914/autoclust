616dda891007b07e197d6233ce845b187e7973a6
Ana Cristina González S. <anacristina0914@me.com>
2025-04-30 10:31:38 +0200

    Mod acr colors functions for clustering

--- R/acr_subgroups_paper_colors.R
+++ R/acr_subgroups_paper_colors.R
@@ -17,9 +17,13 @@ acr_subgroups_paper_colors <- function(
     sg1_var_name = "1", sg2_var_name = "2", sg3_var_name = "3", sg4_var_name = "4",
     fem_var_name = "Female", mas_var_name = "Male", case_var_name = "case", control_var_name = "control"
 ) {
-  colors <- c("#3d7196", "#cf823e", "#438943", "#b34b4c",
+  #colors <- c("#3d7196", "#cf823e", "#438943", "#b34b4c",
+  #            "#B983A7", "#C8D9F2", "#C16840", "#7f7f7f")
+
+  colors <- c("#72a3c7", '#f5cd87', '#588d7a', '#ff876f',
               "#B983A7", "#C8D9F2", "#C16840", "#7f7f7f")
   names(colors) <- c(sg1_var_name, sg2_var_name, sg3_var_name, sg4_var_name,
                      fem_var_name, mas_var_name, case_var_name, control_var_name)
   return(colors)
 }
+
