316ca487a8c20c24318937a30c9c39c51c91e244
Ana Cristina González S. <anacristina0914@me.com>
2025-06-11 11:32:02 +0200

    Working function, added silhouette score barplots for each cluster given by ndim

--- R/cluster_eval.R
+++ R/cluster_eval.R
@@ -8,6 +8,7 @@
 #' @param sillplot_dot_size Numeric. The size of the points in the silhouette plot. Default is 6.
 #' @param save_plot Logical. If TRUE, saves the silhouette plot as a JPEG file. Default is TRUE.
 #' @param nclust Optional. An integer specifying the desired number of clusters. If not provided, the function uses the number of clusters with the highest silhouette width.
+#' @param interactive Logical. If TRUE, the results return interactive silhouette plots for each number of clusters given by ndim.
 #'
 #' @return A list containing:
 #' \item{silhouette_plot}{A ggplot2 object for the silhouette plot.}
@@ -25,9 +26,10 @@
 #' @export
 
 cluster_eval <- function(x, ndim = 10, sillplot_dot_size = 6,
-                         save_plot = TRUE, nclust = NA){
+                         save_plot = TRUE, nclust = NA, interactive = FALSE){
   # Plot silhouette if specified by user
   results <- list()
+  results$silhouette_plots <- list()  # Silhouette plots per k
 
   # Calculate silhouette widths for cluster sizes 2 to ndim
   sil_width <- numeric(ndim-1)
@@ -36,6 +38,33 @@ cluster_eval <- function(x, ndim = 10, sillplot_dot_size = 6,
   for (i in 2:ndim) {
     pam_fit[[as.character(i)]] <- pam(x, diss = TRUE, k = i)
     sil_width[i] <- pam_fit[[as.character(i)]]$silinfo$avg.width
+
+    sil <- silhouette(pam_fit[[as.character(i)]])
+    sil_df <- as.data.frame(sil[, 1:3])  # cluster, neighbor, sil_width
+    sil_df$obs <- 1:nrow(sil_df)
+
+    # Order bars
+    sil_df <- sil_df %>%
+      arrange(cluster, -sil_width, .by_group = TRUE) %>%
+      mutate(order = factor(row_number()))
+
+    # Base ggplot silhouette individual observations
+    p <- ggplot(sil_df, aes(y = order, x = sil_width, fill = factor(cluster))) +
+      geom_bar(stat = "identity", width = 0.8) +
+      scale_fill_carto_d(name = "cluster", palette = "Safe") +
+      labs(x = "Silhouette Width", y = "Observation", fill = "Cluster") +
+      ggtitle(paste("Silhouette plot for k =", i)) +
+      theme_minimal() +
+      theme(axis.text.y = element_blank(),
+            axis.ticks.y = element_blank())
+
+    # Make an interactive plot if interactive = TRUE
+    if (interactive) {
+      p <- ggplotly(p, tooltip = c("x", "fill"))
+    }
+
+    # Store the plot in the list
+    results$silhouette_plots[[paste0(i, "_clusters")]] <- p
   }
 
   # Create silhouette plot with ggplot2
@@ -49,11 +78,11 @@ cluster_eval <- function(x, ndim = 10, sillplot_dot_size = 6,
     theme_linedraw()
 
   # Add silhouette plot to the list
-  results$silhouette_plot <- sillplot
+  results$silhouette_plots$avg_silhouette_plot <- sillplot
 
   # If save_plots = TRUE save them
   if (save_plot){
-    ggsave(plot = sillplot, filename = sprintf("Sillplot_n%s_%s.jpeg", ndim, metric), path =".")
+    ggsave(plot = sillplot, filename = sprintf("Sillplot_n%s.jpeg", ndim), path =".")
     }
 
   # Cluster based on best silhouette
@@ -62,7 +91,10 @@ cluster_eval <- function(x, ndim = 10, sillplot_dot_size = 6,
     n_clust <- sil_data %>%
       arrange(desc(Avg_Silhouette_Width)) %>%
       slice_head(n = 1) %>%
-      pull(Clusters)
+      pull(Clusters) %>%
+      as.numeric()
+  } else {
+    n_clust <- nclust
   }
 
   # Run clustering with best number of clusters
